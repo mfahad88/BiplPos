75113ad88e5836b3374dd40ee573496febd6ce45
fahad <mfahad88@outlook.com>
2017-02-09 13:45:48 +0500

    Start

--- app/src/main/java/com/example/bipl/biplpos/MainActivity.java
+++ app/src/main/java/com/example/bipl/biplpos/MainActivity.java
@@ -1,23 +1,80 @@
 package com.example.bipl.biplpos;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static com.example.bipl.biplpos.R.string.password;
+import static com.example.bipl.biplpos.R.string.username;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText user,pass;
+    private Button btn_login;
+    private ProgressDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
+        user=(EditText)findViewById(R.id.editTextUser);
+        pass=(EditText)findViewById(R.id.editTextPass);
+        btn_login=(Button)findViewById(R.id.buttonLogin);
+        dialog=new ProgressDialog(MainActivity.this);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new loginTask().execute();
+            }
+        });
+    }
+
+    private class loginTask extends AsyncTask<Void,Void,Boolean> {
 
 
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+
+            String username= user.getText().toString();
+            String password= pass.getText().toString();
+            boolean status=false;
+
+            if(username.equals("admin") && password.equals("123")){
+                status=true;
+            }else{
+                status=false;
+            }
+
+            return status;
+        }
+
+           @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if(aBoolean){
+                Toast.makeText(MainActivity.this, "Login Successful...", Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(MainActivity.this,SelectionActivity.class);
+                startActivity(i);
+            }else{
+                Toast.makeText(MainActivity.this, "Login failed!!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/bipl/biplpos/SelectionActivity.java
@@ -0,0 +1,75 @@
+package com.example.bipl.biplpos;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import layout.InventoryFragment;
+import layout.ReportFragment;
+import layout.SalesFragment;
+
+/**
+ * Created by fahad on 2/9/2017.
+ */
+
+public class SelectionActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selection_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new InventoryFragment(), "Inventory");
+        adapter.addFragment(new SalesFragment(), "Sales");
+        adapter.addFragment(new ReportFragment(), "Report");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
